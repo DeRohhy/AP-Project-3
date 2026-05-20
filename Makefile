3a6d329f28de1ba5dd2eeee76cf250a45dc0a831
RohhyDohhy <rohhy.business@gmail.com>
2026-05-20 17:30:16 +0330

    Add simple Makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+a.out: main.o
+	g++ main.o -o a.out
+
+main.o: main.cpp
+	g++ -c main.cpp
+
+clean:
+	rm *.o a.out
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+* ========== Naming Convention Guideline ==========
+* Classes & Enums & Structs: PascalCase
+* Functions: camelCase
+* Variables: lower_snake_case
+* Constants: UPPER_SNAKE_CASE
+* =================================================
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    
+    return 0;
+}
