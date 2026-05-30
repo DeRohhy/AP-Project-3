d3fbb8938e537fc5171083d46c27d822efa14484
RohhyDohhy <rohhy.business@gmail.com>
2026-05-30 18:48:28 +0330

    refactor: removed Subject class from Observer.h due to over engineering. update Observer::onNotify()

--- observer/Observer.h
+++ observer/Observer.h
@@ -2,26 +2,12 @@
 
 #include <vector>
 
+class Component;
+enum class ComponentState;
+
 class Observer
 {
 public:
     virtual ~Observer() {}
-
-    virtual void onNotify() = 0;
-};
-
-class Subject
-{
-public:
-    Subject() {}
-
-    virtual ~Subject() {}
-
-    virtual void addObserver(Observer* observer);
-
-    virtual void removeObserver(Observer* observer);
-
-    virtual void notify();
-private:
-    std::vector<Observer*> observers;
+    virtual void onNotify(const Component* component, ComponentState old_state, ComponentState new_state) = 0;
 };
